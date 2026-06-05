7e566f3f9d3736074d882ddf727e68e016443968
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2026-06-05 21:50:12 +0300

    feat: initialize frontend project and set up basic structure
    - Added favicon.svg for the application icon.
    - Created icons.svg containing various symbols for UI components.
    - Introduced App.css for styling the application layout and components.
    - Developed App.jsx as the main application component with a basic structure.
    - Included index.css to import Tailwind CSS for utility-first styling.
    - Set up main.jsx to render the App component using React's StrictMode.
    - Configured vite.config.js for Vite with React plugin.

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <section id="center">
+      <div className="ticks"></div>
+        
+        <div>
+          <h1>Get started</h1>
+        </div>
+      </section>
+
+
+      <div className="ticks"></div>
+      <section id="spacer"></section>
+    </>
+  )
+}
+
+export default App
