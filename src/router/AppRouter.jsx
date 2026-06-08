b522926076ec93462a194de33a3a3314cb327fbd
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2026-06-08 22:53:04 +0300

    feat: update HeroSection styles, enhance AppRouter with nested routes, and add PrivateRouter component

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,13 +2,18 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from '../pages/Home'
 import Navbar from '../components/home/Navbar'
+import Register from '../pages/Register'
+import Login from '../pages/Login'
 
 const AppRouter = () => {
   return (
     <Router>
       <Navbar/>
       <Routes>
-        <Route path="/" element={<Home/>} />
+        <Route path="/" element={<Home/>}>
+          <Route path="/register" element={<Register/>}/>
+          <Route path="/login" element={<Login/>}/>
+        </Route>
       </Routes>
     </Router>
   )
