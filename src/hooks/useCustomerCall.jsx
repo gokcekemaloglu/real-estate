5ad40eeda00bed0d55a71aa0d08d2af480c37f51
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2026-06-19 14:30:33 +0300

    feat: implement user&customer data fetching and management in useUserCall&useCustomerCall; add error handling and notifications

--- src/hooks/useCustomerCall.jsx
+++ src/hooks/useCustomerCall.jsx
@@ -1,9 +1,74 @@
 import React from 'react'
+import { useDispatch } from 'react-redux';
+import useAxios from './useAxios';
+import { fetchFail, fetchStart, setData } from '../features/customerSlice';
+import { SweetAlertIcons, SweetConfirm, SweetNotify } from '../helper/SweetNotify';
 
 const useCustomerCall = () => {
-  return (
-    <div>useCustomerCall</div>
-  )
+  const dispatch = useDispatch();
+  const axiosWithToken = useAxios();
+
+  const handleError = (error, fallbackMsg) => {
+    const errorMsg = error?.response?.data?.message || fallbackMsg
+    dispatch(fetchFail(errorMsg));
+    SweetNotify(errorMsg, SweetAlertIcons.ERROR);
+  };
+
+  const getSingleCustomerData = async (id) => {
+    dispatch(fetchStart());
+    try {
+      const {data} = await axiosWithToken.get(`customers/${id}`)
+      dispatch(setData({endpoint:"customer", data: data?.data}))
+    } catch (error) {
+      handleError(error, `${id} kimlikli müşteri detayları yüklenemedi!`);
+    }
+  };
+  
+  const postCustomerData = async (endpoint = "Customers", info) => {
+    dispatch(fetchStart());
+    try {
+      await axiosWithToken.post(`${endpoint}`, info);
+      SweetNotify("Müşteri başarıyla yayına alındı.", SweetAlertIcons.SUCCESS)
+    } catch (error) {
+      handleError(error, "Yeni müşteri oluşturulurken hata oluştu!");
+    }
+  };
+  
+  const putCustomerData = async (id, info) => {
+    dispatch(fetchStart());
+    try {
+      await axiosWithToken.put(`customers/${id}`, info);
+      SweetNotify("Müşteri başarıyla güncellendi!", SweetAlertIcons.SUCCESS)
+    } catch (error) {
+      handleError(error, "Müşteri güncellenirken bir hata oluştu!");
+    } finally {
+      getSingleCustomerData(id)
+    }
+  };
+  
+  const deleteCustomer = async (id) => {
+    const confirmed = await SweetConfirm("Sil", "Silmek istediğinize emin misiniz?", SweetAlertIcons.QUESTION)
+    if (!confirmed) return //Exit if user cancels delete
+    dispatch(fetchStart());
+    try {
+      await axiosWithToken.delete(`customers/${id}`)
+      SweetNotify("Müşteri başarıyla silindi!", SweetAlertIcons.SUCCESS)
+    } catch (error) {
+      handleError(error, "Müşteri silinirken bir hata oluştu!");
+    }
+  };
+    
+  const toggleCustomerStatus = async (id) => {
+    dispatch(fetchStart());
+    try {
+      await axiosWithToken.patch(`customers/${id}/status`);
+      SweetNotify("Yayın durumu başarıyla güncellendi!", SweetAlertIcons.SUCCESS)
+    } catch (error) {
+      handleError(error, "Müşteri aktiflik durumu değiştirilirken bir hata oluştu!");
+    }
+  };
+
+  return {getSingleCustomerData, postCustomerData, putCustomerData, deleteCustomer, toggleCustomerStatus}
 }
 
 export default useCustomerCall
--- src/hooks/useUserCall.jsx
+++ src/hooks/useUserCall.jsx
@@ -1,9 +1,66 @@
-import React from 'react'
+import { useDispatch } from 'react-redux'
+import useAxios from './useAxios'
+import { fetchFail, fetchStart, setData } from '../features/userSlice'
+import { logoutSuccess } from '../features/authSlice'
+import { useNavigate } from 'react-router-dom'
+import { SweetAlertIcons, SweetNotify } from '../helper/SweetNotify'
 
 const useUserCall = () => {
-  return (
-    <div>useUserCall</div>
-  )
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const axiosWithToken = useAxios()
+
+  const handleError = (error, fallbackMsg) => {
+    const errorMsg = error?.response?.data?.message || fallbackMsg
+    dispatch(fetchFail(errorMsg));
+    SweetNotify(errorMsg, SweetAlertIcons.ERROR);
+  };  
+  
+  const getSingleUser = async(id) => {
+    dispatch(fetchStart())
+    try {
+      const {data} = await axiosWithToken.get(`users/${id}`)
+      dispatch(setData(data.data))
+    } catch (error) {
+      dispatch(fetchFail())
+      handleError(error, `${id} kimlikli kullanıcı detayları yüklenemedi!`);
+    }
+  }
+
+  // Update User's own profile
+  const updateMe = async (id, userUpdateInfo) => {
+    dispatch(fetchStart())
+    try {
+      await axiosWithToken.patch(`users/${id}/updateMe`, userUpdateInfo)
+      SweetNotify("Kullanıcı bilgileri başarıyla güncellendi!", SweetAlertIcons.SUCCESS)
+    } catch (error) {
+      dispatch(fetchFail())
+      handleError(error, "Kulllanıcı hesabı güncellenirken bir hata oluştu!");
+    } finally {
+      getSingleUser(id)
+    }
+  }
+
+  // Change User's own password
+  const changeMyPassword = async (id, values) => {
+    dispatch(fetchStart())
+    try {
+      await axiosWithToken.patch(`users/${id}/changeMyPassword`, values)
+      SweetNotify("Password changed successfully!!", SweetAlertIcons.SUCCESS)
+      await axiosWithToken.get("auth/logout")
+      dispatch(logoutSuccess())
+      navigate("/login")
+    } catch (error) {
+      dispatch(fetchFail())
+      handleError(error, "Şifre değiştirilirken bir hata oluştu!");
+    }
+  }
+  
+  return {
+    updateMe,
+    getSingleUser,
+    changeMyPassword
+  }
 }
 
 export default useUserCall
