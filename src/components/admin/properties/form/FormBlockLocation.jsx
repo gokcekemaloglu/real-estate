55ab9385be5cc3f29c07eac7c504de8de495b313
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2026-07-03 22:12:55 +0300

    feat: refactor FormBlockLocation to use FormInput for city and neighbourhood fields, enhancing consistency and code clarity

--- src/components/admin/properties/form/FormBlockLocation.jsx
+++ src/components/admin/properties/form/FormBlockLocation.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import FormSelectField from "./FormSelectField";
+import FormInput from "../../../auth/FormInput";
 
 const FormBlockLocation = ({formik, districtOptions}) => {
   return (
@@ -7,19 +8,14 @@ const FormBlockLocation = ({formik, districtOptions}) => {
       <h3 className="md:col-span-3 text-xs uppercase tracking-widest text-brand-gold font-medium border-b border-slate-100 dark:border-slate-800/60 pb-2">
         3. Konum Bilgileri
       </h3>
-
-      <div className="flex flex-col gap-1.5">
-        <label className="text-[10px] uppercase tracking-wider text-slate-400 font-medium">
-          Şehir
-        </label>
-        <input
-          type="text"
-          name="city"
-          value={formik.values.city}
-          disabled
-          className="input-premium bg-slate-100 dark:bg-slate-800/30 opacity-60 cursor-not-allowed"
-        />
-      </div>
+      <FormInput
+        label="Şehir"
+        name="city"
+        type="text"
+        placeholder=""
+        formikProps={formik}
+        disabled={true}
+      />
 
       <FormSelectField
         label="İlçe"
@@ -27,26 +23,14 @@ const FormBlockLocation = ({formik, districtOptions}) => {
         options={districtOptions}
         formik={formik}
       />
-
-      <div className="flex flex-col gap-1.5">
-        <label className="text-[10px] uppercase tracking-wider text-slate-400 font-medium">
-          Mahalle
-        </label>
-        <input
-          type="text"
-          name="neighbourhood"
-          placeholder="Cemalpaşa"
-          value={formik.values.neighbourhood}
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          className={`input-premium bg-slate-50/50 dark:bg-slate-950/20 ${formik.touched.neighbourhood && formik.errors.neighbourhood ? "border-red-500 focus:border-red-500" : ""}`}
-        />
-        {formik.touched.neighbourhood && formik.errors.neighbourhood && (
-          <span className="text-[11px] text-red-500 font-light mt-1">
-            {formik.errors.neighbourhood}
-          </span>
-        )}
-      </div>
+      <FormInput
+        label="Mahalle"
+        name="neighbourhood"
+        placeholder="Mahalle..."
+        formikProps={formik}
+        required= {true}
+        disabled={formik.isSubmitting}
+      />
     </div>
   );
 };
