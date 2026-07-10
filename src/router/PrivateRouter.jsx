46dcdb8cc38bcc4fedcf850172fbc4a2bf68e632
gokcekemaloglu <gokce.kemaloglu@gmail.com>
2026-07-10 15:02:20 +0300

    feat: implement MyFavorites page and integrate with favorites functionality

--- src/router/PrivateRouter.jsx
+++ src/router/PrivateRouter.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
+import { Navigate, Outlet } from 'react-router-dom';
+import { SweetAlertIcons, SweetNotify } from '../helper/SweetNotify';
 
 const PrivateRouter = () => {
-  return (
-    <div>PrivateRouter</div>
-  )
+  const {token} = useSelector((state) => state.auth);
+  if (!token) {
+    SweetNotify("Bu sayfaya erişebilmek için lütfen önce giriş yapınız.", SweetAlertIcons.WARNING);
+    return <Navigate to="/login" replace />
+  }
+  return <Outlet />
 }
 
 export default PrivateRouter
